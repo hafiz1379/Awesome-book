a7f2bf0cf998256b483859187af0b477ee8884df
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-09 13:52:04 +0430

    Solved linter errors

--- modules/book.js
+++ modules/book.js
@@ -1,8 +1,8 @@
 class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  
-  export default Book;
+}
+
+export default Book;
